a82309c95c90fd76c8c385107a9c1be05dc9aa8a
allen <allen-jx@jxinternet.com>
2017-11-08 16:10:07 +0800

    skill Tree
    学习思维导图

---
+++ PythonProject/ex1.py
@@ -0,0 +1,31 @@
+# -*- coding:utf-8 -*-
+# print "Hello World!"
+# print "Hello Again"
+# print "I like typing this."
+# print "This is fun."
+# print 'Yay! Printing.'
+# print "I'd much rather you 'not'."
+# print 'I "said" do not touch this.'
+# print "大家好！".decode("utf-8")
+cars = 100.0
+print "there are",cars,"cars available."
+my_name = 'Allen'
+my_age = 26
+
+print "My name is %s,I'm %d old."%(my_name,my_age)
+print "."*10
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
+import this
